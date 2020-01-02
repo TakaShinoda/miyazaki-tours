707c9dddca571dc3c159bad0eb7df2980524a4f8
Takahiro <tkhr_621@docomo.ne.jp>
2020-01-03 03:40:59 +0900

    広報ツイートの表示

--- src/components/Tourlist.vue
+++ src/components/Tourlist.vue
@@ -7,11 +7,41 @@
       <p>
         <router-link to="/horikiri">堀切峠</router-link>
       </p>
+      <table border="1" width="500" align="center" cellspacing="0" cellpadding="1">
+        <tr>
+          <td>
+            <div class="twitter-widget">
+              <v-timeline
+                :id="twitterId"
+                :source-type="'profile'"
+                :options="{ 'height': twitterHeight }"
+              />
+            </div>
+          </td>
+        </tr>
+      </table>
       <p>
         <router-link to="/signout">ログアウト</router-link>
       </p>
   </div>
 </template>
+
+<script>
+import { Tweet, Moment, Timeline } from 'vue-tweet-embed'
+
+export default {
+  name: 'Tourlist',
+  components: {
+    'v-timeline': Timeline,
+  },
+  data () {
+    return {
+      twitterId: 'miyazakipref',
+      twitterHeight: '450',
+    }
+  },
+}
+</script>
  
 <style>
 </style>
