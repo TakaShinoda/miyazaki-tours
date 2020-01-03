03c93b2712a71ed76c2ef2c103c65bf3b3f47ba0
Takahiro <tkhr_621@docomo.ne.jp>
2020-01-03 15:35:14 +0900

    匿名ログインの案内を追加

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
   <div id="home">
       <h2>Home</h2>
       <p>
-        <router-link to="/signin">ログイン</router-link>
+        <router-link to="/signin">ログイン(匿名ログインできます)</router-link>
       </p>
       <p>
         <router-link to="/signup">新規作成</router-link>
